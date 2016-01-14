dcd42cb861f94f8ddc58c16fce0d17edb58c2475
stansL <@dmin2@14!>
2016-01-14 07:41:07 +0300

    update to project structure configurations to comply with uiframework

---
+++ omod/src/main/java/org/openmrs/module/tbclinic/fragment/controller/HomeFragmentController.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.tbclinic.fragment.controller;
+
+/**
+ * @author  Stanslaus Odhiambo
+ * Created on 1/14/2016.
+ *
+ */
+public class HomeFragmentController {
+
+    /**
+     * Default handler for POST and GET requests if none is provided
+     */
+    public void controller() {
+
+    }
+
+
+}
---
+++ omod/src/main/java/org/openmrs/module/tbclinic/page/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.tbclinic.page.controller;
+
+/**
+ * @author  Stanslaus Odhiambo
+ * Created on 1/14/2016.
+ *
+ */
+public class HomePageController {
+
+    /**
+     * Default handler for POST and GET requests if none is provided
+     */
+    public void controller() {
+
+    }
+}
